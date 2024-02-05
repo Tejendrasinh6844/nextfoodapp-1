007422717868d05390c93b0989331647fd9fb7fa
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-05 21:26:58 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import Menu from './Menu'
+import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className=' h-12 text-red-500 p-4 flex justify-between items-center border-b-2 boder-b-red-500 uppercase' >
+      {/* logo */}
+      <div className='text-xl'>
+        <Link href="/">
+        Massimo
+        </Link>
+      </div>
+      {/* mobile-menu */}
+      <div>
+        <Menu/>
+      </div>
     </div>
   )
 }
