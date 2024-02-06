7fd5ae93531944a9a4a7bfd92a8ee18217eb62e3
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-06 09:22:37 +0530

    3rd comment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import Menu from './Menu'
 import Link from 'next/link'
+import CartIcon from './CartIcon'
+import Image from 'next/image'
 const Navbar = () => {
+  const user = false
   return (
-    <div className=' h-12 text-red-500 p-4 flex justify-between items-center border-b-2 boder-b-red-500 uppercase' >
+    <div className=' h-12 text-red-500 p-4 flex justify-between items-center border-b-2 boder-b-red-500 uppercase md:h-24 ' >
+      {/* left links */}
+      <div className="hidden md:flex gap-4  flex-1">
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+
       {/* logo */}
-      <div className='text-xl'>
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
         <Link href="/">
-        Massimo
+          {/* Massimo */}
+          Tejendrasinh
         </Link>
       </div>
       {/* mobile-menu */}
-      <div>
-        <Menu/>
+      <div className='md:hidden'>
+        <Menu />
+      </div>
+      {/* right links */}
+      <div className="hidden md:flex gap-4 items-center flex-1 ">
+        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+          <Image src="/phone.png" alt='' width={20} height={20}/>
+          <span>7226866844</span>
+        </div>
+       {!user ?  <Link href="/login">Login</Link> :
+        <Link href="/orders">Orders</Link>}
+        <CartIcon/>
       </div>
     </div>
   )
